30a632148698de21e2f14ead9634e53e56824930
kunkelchristian <131902263+kunkelchristian@users.noreply.github.com>
2023-10-09 21:20:00 +0200

    Fertig :-)

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -11,13 +11,30 @@ namespace ContosoPizza.Pages
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
-        
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
         public PizzaListModel(PizzaService service)
         {
             _service = service;
         }
-        
 
+        public IActionResult OnPostDelete(int id){
+            _service.DeletePizza(id);
+
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToAction("Get");
+        }
         public void OnGet()
         {
             PizzaList = _service.GetPizzas();
